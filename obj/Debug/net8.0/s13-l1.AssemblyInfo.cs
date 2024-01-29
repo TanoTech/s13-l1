db730732f356002dc19ff2e4d97f7a88d37dae7a
GaetanoNapoli <gaetano.napoli@hotmail.com>
2024-01-29 16:40:29 +0100

    prova 2

--- obj/Debug/net8.0/s13-l1.AssemblyInfo.cs
+++ obj/Debug/net8.0/s13-l1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("s13-l1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f416c987cd9342f2ea0969d410a6bebcbe0998fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d241b90a674181ff21b2b1d7fdcfc0d98b370a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("s13-l1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("s13-l1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
